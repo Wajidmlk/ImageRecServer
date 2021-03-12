184838dc55eb63e2478573f4dfe59e1063b13b4d
wajidmlk <wajidmlk123@gmail.com>
2021-03-13 00:50:36 +0500

    Database Set

--- server.js
+++ server.js
@@ -10,10 +10,8 @@ const image = require('./controllers/image');
 const db= knex({
   client: 'pg',
   connection: {
-    host : 'localhost',
-    user : 'postgres',
-    password : '61ba5678',
-    database : 'smart-brain'
+   host:process.env.DATABASE_URL,
+   ssll:true
   }
 });
 
